c0e2cf700bc404bf801f4ea715fdfea61bff32f1
QuickBundle <ypponline@gmail.com>
2026-06-22 20:03:22 +0700

    fix: restore unstable_newEmbeddedAuthStrategy to fix cookie error in iframe

--- app/shopify.server.ts
+++ app/shopify.server.ts
@@ -16,6 +16,9 @@ const shopify = shopifyApp({
   authPathPrefix: "/auth",
   sessionStorage: new PrismaSessionStorage(prisma),
   distribution: AppDistribution.AppStore,
+  future: {
+    unstable_newEmbeddedAuthStrategy: true,
+  },
   ...(process.env.SHOP_CUSTOM_DOMAIN
     ? { customShopDomains: [process.env.SHOP_CUSTOM_DOMAIN] }
     : {}),
